a4bae0fa557865e0a9658761817166715bd074c9
Thanh Dương <thanhteo290301@gmail.com>
2022-06-21 21:07:22 +0700

    pull UI

--- tasks/views.py
+++ tasks/views.py
@@ -34,20 +34,34 @@ def list_view(request, category_id):
 
 
 def detail_view(request, category_id, task_id):
+    keyword = request.GET.get('keyword')
+    sort = request.GET.get('sort')
+    if keyword == "None":
+        keyword = ''
+    if sort == "None":
+        sort = ''
     task = Task.objects.get(id=task_id)
     user = request.user if request.user.is_authenticated else None
     tasks = Task.objects.all()
+    if keyword:
+        tasks = Task.objects.filter(name__icontains = keyword)
     if category_id != 3:
         tasks = tasks.filter(category__id = category_id)
     tasks = tasks.filter(user=user)
+    if sort=="1":
+        tasks = tasks.order_by('name').values()
+    if sort=="2":
+        tasks = tasks.order_by('complete').values()
     count_complete = tasks.filter(complete = True).count()
     count_incomplete = tasks.filter(complete = False).count()
     context = {
+        'keyword': keyword,
         'task': task,
         'tasks': tasks,
         'category_id': category_id,
         'count_complete': count_complete,
         'count_incomplete': count_incomplete,
+        'typesort': sort,
     }
     return render(request, 'list.html', context)
 
@@ -55,6 +69,10 @@ def complete_view(request, category_id, task_id):
     task = Task.objects.get(id=task_id)
     sort = request.GET.get('sort')
     keyword = request.GET.get('keyword')
+    if keyword == "None":
+        keyword = ''
+    if sort == "None":
+        sort = ''
     user = request.user if request.user.is_authenticated else None
     tasks = Task.objects.all()
     task.complete = not task.complete
@@ -79,4 +97,4 @@ def complete_view(request, category_id, task_id):
         'count_complete': count_complete,
         'count_incomplete': count_incomplete,
     }
-    return render(request, 'list.html', context)
+    return redirect(f"/list/{category_id}?keyword={keyword}&sort={sort}")
