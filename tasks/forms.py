3161c9b067e62b8d5ff79a8db475e0784fbc0d70
Dang Kiet Nguyen <kietnguyen17052001@gmail.com>
2022-06-22 16:02:08 +0700

    deploy project

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Task
 class TaskForm(forms.Form):
-    name = forms.CharField(label='', required=False, 
-                           widget=forms.TextInput(attrs={'class':'form-control'}),
+    name = forms.CharField(label='', required=True, 
+                           widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter new task...'}),
                            max_length=255)
     # class Meta:
     #     model = Task
